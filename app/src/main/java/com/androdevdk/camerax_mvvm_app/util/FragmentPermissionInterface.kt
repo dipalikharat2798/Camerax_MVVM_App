91b67efe57f9d20c6a1dd02e5b88b2ea4912be08
Dipali <dipalikharat2798@gmail.com>
2022-03-04 11:03:18 +0530

    last commit

--- app/src/main/java/com/androdevdk/camerax_mvvm_app/util/FragmentPermissionInterface.kt
+++
@@ -1,5 +0,0 @@
-package com.androdevdk.camerax_mvvm_app.util
-
-interface FragmentPermissionInterface {
-   fun onGranted(isgranted:Boolean)
-}
--- app/src/main/java/com/androdevdk/camerax_mvvm_app/view/fragments/PermissionsFragment.kt
+++ app/src/main/java/com/androdevdk/camerax_mvvm_app/view/fragments/PermissionsFragment.kt
@@ -7,67 +7,41 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentActivity
 import com.androdevdk.camerax_mvvm_app.R
-import com.androdevdk.camerax_mvvm_app.util.FragmentPermissionHelper
-import com.androdevdk.camerax_mvvm_app.util.FragmentPermissionInterface
 
 private const val PERMISSIONS_REQUEST_CODE = 10
 private val PERMISSIONS_REQUIRED = arrayOf(Manifest.permission.CAMERA)
 
 @Suppress("DEPRECATION")
 class PermissionsFragment : Fragment() {
-    lateinit var fr: FragmentActivity
-
-    override fun onAttach(context: Context) {
-        super.onAttach(context)
-        fr = context as FragmentActivity
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        FragmentPermissionHelper().startPermissionRequest(
-            fr,
-            object : FragmentPermissionInterface {
-                override fun onGranted(isgranted: Boolean) {
-                    if (isgranted) {
-                        navigateToCamera()
-                    } else {
-                        // Explain to the user that the feature is unavailable because the
-                        // features requires a permission that the user has denied. At the
-                        // same time, respect the user's decision. Don't link to system
-                        // settings in an effort to convince the user to change their
-                        // decision.
-                    }
-                }
-            },
-            Manifest.permission.CAMERA
-        );
-        /*  if (!hasPermissions(requireContext())) {
-              // Request camera-related permissions
-              requestPermissions(PERMISSIONS_REQUIRED, PERMISSIONS_REQUEST_CODE)
-          } else {
-              // If permissions have already been granted, proceed
-              navigateToCamera()
-          }
-         */
+
+        if (!hasPermissions(requireContext())) {
+            // Request camera-related permissions
+            requestPermissions(PERMISSIONS_REQUIRED, PERMISSIONS_REQUEST_CODE)
+        } else {
+            // If permissions have already been granted, proceed
+            navigateToCamera()
+        }
     }
 
-    /* override fun onRequestPermissionsResult(
-         requestCode: Int, permissions: Array<String>, grantResults: IntArray
-     ) {
-         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-         if (requestCode == PERMISSIONS_REQUEST_CODE) {
-             if (PackageManager.PERMISSION_GRANTED == grantResults.firstOrNull()) {
-                 // Take the user to the success fragment when permission is granted
-                 Toast.makeText(context, "Permission request granted", Toast.LENGTH_LONG).show()
-                 navigateToCamera()
-             } else {
-                 Toast.makeText(context, "Permission request denied", Toast.LENGTH_LONG).show()
-             }
-         }
-     }
- */
+    override fun onRequestPermissionsResult(
+        requestCode: Int, permissions: Array<String>, grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == PERMISSIONS_REQUEST_CODE) {
+            if (PackageManager.PERMISSION_GRANTED == grantResults.firstOrNull()) {
+                // Take the user to the success fragment when permission is granted
+                Toast.makeText(context, "Permission request granted", Toast.LENGTH_LONG).show()
+                navigateToCamera()
+            } else {
+                Toast.makeText(context, "Permission request denied", Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
     private fun navigateToCamera() {
         var fragment = CameraFragment()
         var fragmentManager = requireActivity().supportFragmentManager
@@ -78,6 +52,7 @@ class PermissionsFragment : Fragment() {
     }
 
     companion object {
+
         /** Convenience method used to check if all permissions required by this app are granted */
         fun hasPermissions(context: Context) = PERMISSIONS_REQUIRED.all {
             ContextCompat.checkSelfPermission(context, it) == PackageManager.PERMISSION_GRANTED
